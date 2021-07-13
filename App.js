ccf1b95ecb16c9484ca888418eef8b57e8707dcc
Nishanth Bhat <nishanthbhat07@gmail.com>
2021-07-13 16:17:32 +0530

    React native opencv starter code

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import CameraScreen from './src/Screens/CameraScreen';
+
+export default class App extends Component {
+  render() {
+    return <CameraScreen />;
+  }
+}
