96e0f2da00a75cca40d1ad0bef038716ebfbc91e
Nishanth Bhat <nishanthbhat07@gmail.com>
2021-07-21 12:54:45 +0530

    Configured the app.json to build the android apk.

--- android/app/src/main/java/com/opencvreact12/MainApplication.java
+++ android/app/src/main/java/com/opencvreact12/MainApplication.java
@@ -37,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            
             new SafeAreaContextPackage(),
                     new SvgPackage(),
                     new RNCameraPackage(),
